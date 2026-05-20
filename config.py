22c6d78b0f354e426faead2a64adfab7dd048074
wmckenzieGP <wmckenzie@sailgpnzl.com>
2026-05-20 11:04:20 +1000

    Update config.py
    Updated for online version

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
-import dotenv
 
-dotenv.load_dotenv()
-
-ORG_ID = os.getenv('ORG_ID')
-TOKEN = os.getenv('TOKEN')
-URL = os.getenv('URL')
+try:
+    import streamlit as st
+    # 1. When running on the internet, this looks in your Streamlit Cloud vault
+    ORG_ID = st.secrets["ORG_ID"]
+    TOKEN = st.secrets["TOKEN"]
+    URL = st.secrets["URL"]
+except:
+    # 2. When running on your laptop, the cloud vault isn't there, so it falls back to your local setup
+    import dotenv
+    dotenv.load_dotenv()
+    
+    ORG_ID = os.getenv('ORG_ID')
+    TOKEN = os.getenv('TOKEN')
+    URL = os.getenv('URL')
